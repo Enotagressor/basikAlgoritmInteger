b11c81341597d9e58e10d0ac67887e227cbc092a
Sergey <enotagressor@gmail.com>
2022-08-10 22:21:31 +0700

    1

---
+++ basikAlgoritmInteger/src/main/java/pro/ski/basikAlgoritmInteger/BasikAlgoritmIntegerApplication.java
@@ -0,0 +1,13 @@
+package pro.ski.basikAlgoritmInteger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasikAlgoritmIntegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasikAlgoritmIntegerApplication.class, args);
+	}
+
+}
---
+++ basikAlgoritmInteger/src/main/java/pro/ski/basikAlgoritmInteger/service/IntegerListImpl.java
@@ -0,0 +1,184 @@
+package pro.ski.basikAlgoritmInteger.service;
+
+import pro.ski.basikAlgoritmInteger.exception.ExceptionInvalidIndex;
+import pro.ski.basikAlgoritmInteger.exception.ExceptionNotFound;
+import pro.ski.basikAlgoritmInteger.exception.ExceptionNull;
+import pro.ski.basikAlgoritmInteger.exception.ExceptionSize;
+
+import java.util.Arrays;
+
+public class IntegerListImpl implements IntegerList {
+    private Integer[] storage;
+    private int size;
+
+    public IntegerListImpl() {
+        storage = new Integer[10];
+    }
+
+    public IntegerListImpl(int initSize) {
+        storage = new Integer[initSize];
+    }
+
+    private void validItem(Integer item) {
+        if (item == null) {
+            throw new ExceptionNull();
+        }
+    }
+
+    private void validSize() {
+        if (size == storage.length) {
+            throw new ExceptionSize();
+        }
+    }
+
+    private void validIndex(int index) {
+        if (index < 0 || index > size) {
+            throw new ExceptionInvalidIndex();
+        }
+    }
+    public static void sort(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+    private boolean binarySearch(Integer[] arr, Integer item) {
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (item == arr[mid]) {
+                return true;
+            }
+
+            if (item < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Integer add(Integer item) {
+        validSize();
+        validItem(item);
+        storage[size++]=item;
+        return item;
+    }
+
+    @Override
+    public Integer add(int index, Integer item) {
+        validSize();
+        validItem(item);
+        validIndex(index);
+        if(index == size){
+            storage[size++]=item;
+            return item;
+        }
+        System.arraycopy(storage, index, storage, index + 1, size - index);
+        storage[index] = item;
+        size++;
+        return item;
+    }
+
+    @Override
+    public Integer set(int index, Integer item) {
+        validIndex(index);
+        validItem(item);
+        storage[index] = item;
+        return item;
+    }
+
+    @Override
+    public Integer remove(Integer item) {
+        validItem(item);
+        int index = indexOf(item);
+        if(index == -1){
+            throw new ExceptionNotFound();
+        }
+        if (index != size) {
+            System.arraycopy(storage, index + 1, storage, index, size - index);
+        }
+        size--;
+        return item;
+    }
+
+    @Override
+    public Integer remove(int index) {
+        validIndex(index);
+        Integer item = storage[index];
+        if (index != size) {
+            System.arraycopy(storage, index + 1, storage, index, size - index);
+        }
+        size--;
+        return item;
+    }
+
+    @Override
+    public boolean contains(Integer item) {
+        Integer[] storageCopy = toArray();
+        sort(storageCopy);
+        return binarySearch(storageCopy, item);
+    }
+
+
+    @Override
+    public int indexOf(Integer item) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Integer item) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public Integer get(int index) {
+        validIndex(index);
+        return storage[index];
+    }
+
+    @Override
+    public boolean equals(IntegerList otherList) {
+        return Arrays.equals(toArray(), otherList.toArray());
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    }
+
+    @Override
+    public Integer[] toArray() {
+        return Arrays.copyOf(storage, size);
+    }
+}
